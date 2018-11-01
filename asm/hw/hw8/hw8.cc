a315e96681b96ebaea3a4898d87d4d49fae4f7fd
Dov Kruger <Dov.Kruger@gmail.com>
2018-11-01 16:36:20 -0400

    yet again UGH!

--- asm/hw/hw8/hw8.cc
+++ asm/hw/hw8/hw8.cc
@@ -5,7 +5,7 @@ extern uint32_t eratosthenes(uint32_t primeBits[], uint32_t n);
 
 int main() {
 	uint32_t n = 1000000000;
-	uint32_t* sieve = new uint64_t[(n+31)/32];
+	uint32_t* sieve = new uint32_t[(n+31)/32];
 	uint32_t count = eratosthenes(sieve, n);
 	cout << "Number of primes up to " << n << " = " << count << '\n';
 	delete [] sieve;
