3087ed606c161e7b25d13994c86af98dcc17b935
Dov Kruger <Dov.Kruger@gmail.com>
2018-11-08 16:45:22 -0500

    fixed mangled name of eratosthenes for asm code

---
+++ asm/hw/hw8/hw8.s
@@ -0,0 +1,102 @@
+	.file	"hw8.cc"
+	.text
+	.p2align 4,,15
+	.def	__tcf_0;	.scl	3;	.type	32;	.endef
+	.seh_proc	__tcf_0
+__tcf_0:
+.LFB1970:
+	.seh_endprologue
+	leaq	_ZStL8__ioinit(%rip), %rcx
+	jmp	_ZNSt8ios_base4InitD1Ev
+	.seh_endproc
+	.def	__main;	.scl	2;	.type	32;	.endef
+	.section .rdata,"dr"
+.LC0:
+	.ascii "Number of primes up to \0"
+.LC1:
+	.ascii " = \0"
+	.section	.text.startup,"x"
+	.p2align 4,,15
+	.globl	main
+	.def	main;	.scl	2;	.type	32;	.endef
+	.seh_proc	main
+main:
+.LFB1528:
+	pushq	%rdi
+	.seh_pushreg	%rdi
+	pushq	%rsi
+	.seh_pushreg	%rsi
+	pushq	%rbx
+	.seh_pushreg	%rbx
+	subq	$48, %rsp
+	.seh_stackalloc	48
+	.seh_endprologue
+	call	__main
+	movl	$125000000, %ecx
+	call	_Znay
+	movl	$1000000000, %edx
+	movq	%rax, %rcx
+	movq	%rax, %rbx
+	call	_Z12eratosthenesPjj
+	movq	.refptr._ZSt4cout(%rip), %rcx
+	leaq	.LC0(%rip), %rdx
+	movl	$23, %r8d
+	movl	%eax, %edi
+	call	_ZSt16__ostream_insertIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_PKS3_x
+	movq	.refptr._ZSt4cout(%rip), %rcx
+	movl	$1000000000, %edx
+	call	_ZNSo9_M_insertImEERSoT_
+	leaq	.LC1(%rip), %rdx
+	movq	%rax, %rsi
+	movl	$3, %r8d
+	movq	%rax, %rcx
+	call	_ZSt16__ostream_insertIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_PKS3_x
+	movl	%edi, %edx
+	movq	%rsi, %rcx
+	call	_ZNSo9_M_insertImEERSoT_
+	leaq	47(%rsp), %rdx
+	movl	$1, %r8d
+	movq	%rax, %rcx
+	movb	$10, 47(%rsp)
+	call	_ZSt16__ostream_insertIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_PKS3_x
+	movq	%rbx, %rcx
+	call	_ZdaPv
+	xorl	%eax, %eax
+	addq	$48, %rsp
+	popq	%rbx
+	popq	%rsi
+	popq	%rdi
+	ret
+	.seh_endproc
+	.p2align 4,,15
+	.def	_GLOBAL__sub_I_main;	.scl	3;	.type	32;	.endef
+	.seh_proc	_GLOBAL__sub_I_main
+_GLOBAL__sub_I_main:
+.LFB1971:
+	subq	$40, %rsp
+	.seh_stackalloc	40
+	.seh_endprologue
+	leaq	_ZStL8__ioinit(%rip), %rcx
+	call	_ZNSt8ios_base4InitC1Ev
+	leaq	__tcf_0(%rip), %rcx
+	addq	$40, %rsp
+	jmp	atexit
+	.seh_endproc
+	.section	.ctors,"w"
+	.align 8
+	.quad	_GLOBAL__sub_I_main
+.lcomm _ZStL8__ioinit,1,1
+	.ident	"GCC: (Rev2, Built by MSYS2 project) 6.2.0"
+	.def	_ZNSt8ios_base4InitD1Ev;	.scl	2;	.type	32;	.endef
+	.def	_Znay;	.scl	2;	.type	32;	.endef
+	.def	_Z12eratosthenesPjj;	.scl	2;	.type	32;	.endef
+	.def	_ZSt16__ostream_insertIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_PKS3_x;	.scl	2;	.type	32;	.endef
+	.def	_ZNSo9_M_insertImEERSoT_;	.scl	2;	.type	32;	.endef
+	.def	_ZdaPv;	.scl	2;	.type	32;	.endef
+	.def	_ZNSt8ios_base4InitC1Ev;	.scl	2;	.type	32;	.endef
+	.def	atexit;	.scl	2;	.type	32;	.endef
+	.section	.rdata$.refptr._ZSt4cout, "dr"
+	.globl	.refptr._ZSt4cout
+	.linkonce	discard
+.refptr._ZSt4cout:
+	.quad	_ZSt4cout
