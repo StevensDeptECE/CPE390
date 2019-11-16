ac3333ab500f22e5f872cc2e433e9ab86f135783
Dov Kruger <Dov.Kruger@gmail.com>
2019-11-16 10:44:37 -0500

    fixed corrupted repo and added lessons on bit shifting and the benchmark code we have been doing. Stack is in but not yet covered

--- asm/benchmark/benchmark.cc
+++ asm/benchmark/benchmark.cc
@@ -2,10 +2,13 @@
 using namespace std;
 
 extern void countup(uint32_t n);
-
 extern void countdown(uint32_t n);
+extern void multiply(uint32_t n);
+
 void visit(int x[], int32_t n);
 void readwrite(int x[], int32_t n);
+void read1(int x[], int32_t n);
+void readskip(int x[], int32_t n);
 
 typedef void (*BenchFunc)(uint32_t);
 void benchmark(const char msg[], BenchFunc b, uint32_t n) {
@@ -24,13 +27,16 @@ void benchmark2(const char msg[], BenchFunc2 b, int x[], uint32_t n) {
 }
 
 int main() {
-	const uint32_t n = 10000000;
+	const uint32_t n = 1024*1024 * 16;
   benchmark("count up", countup, n);
   benchmark("count down", countdown, n);
+	benchmark("multiply", multiply, n);
 
 	//	int a[size];
 	int* p = new int[n];
 	benchmark2("visit", visit, p, n);
 	benchmark2("readwrite", readwrite, p, n);
+	benchmark2("read1", read1, p, n);
+	benchmark2("readskip", readskip, p, n);
 	delete [] p;
 }
--- asm/benchmark/bunchoffuncs.s
+++ asm/benchmark/bunchoffuncs.s
@@ -16,6 +16,22 @@ _Z9countdownj:
 	bgt .loop2
 	bx lr
 
+	.global _Z8multiplyj
+_Z8multiplyj:
+.loop8:
+	mul	r1,#2
+	subs r0, #1
+	bgt .loop8
+	bx lr
+
+		.global _Z6addingj
+_Z6addingj:
+.loop8:
+0	add	r1,r1
+	subs r0, #1
+	bgt .loop8
+	bx lr
+
 
 	.global _Z5visitPii
 _Z5visitPii:	
@@ -38,3 +54,38 @@ _Z9readwritePii:
 	bgt .loop4
 	bx lr
 
+	.global _Z5read1Pii
+_Z5read1Pii:	
+.loop5:
+	ldr	r2, [r0]
+	subs r1, #1
+	bgt .loop3
+	bx lr
+	
+
+	.global _Z8readskipPii
+_Z8readskipPii:
+	push {r4,r5	}
+	
+	mov	r3, r0
+	mov	r4, r0
+	add	r4, #60
+	mov	r5, r1
+	.loop7:	
+	mov	r0, r3
+	mov	r1, r5
+	.loop6:
+	ldr	r2, [r0]
+	add	r0, #64	
+	subs r1, #16
+	bgt .loop6
+	add	r3, #4
+	cmp	r3, r4
+	ble	.loop7
+
+	pop {r4,r5}
+	bx lr
+
+	
+
+	
