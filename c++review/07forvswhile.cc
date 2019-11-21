399cf467c20e03087a26accd5d70860fd10b2ccd
Dov Kruger <Dov.Kruger@gmail.com>
2019-11-21 16:25:34 -0500

    primes

--- c++review/07forvswhile.cc
+++ c++review/07forvswhile.cc
@@ -1,10 +1,20 @@
 #include <iostream>
 using namespace std;
 
-int f(int n) {
+int f(uint32_t n) {
 	int sum = 0;
 	for (int i = 1; i <= n; i++)
 		sum += i;
+	return sum;
+}
+
+int g(uint32_t n) {
+	int sum = 0;
+	int i = 1;
+	while (i <= n) {
+		sum += i;
+		i++;
+	}
 	return sum;
 }
 
@@ -16,5 +26,9 @@ int add(int a, int b) {
 int main() {
 	int n;
 	cin >> n;
-	cout << f(n);
+	cout << f(n) << flush;
+	int x = 0;
+	cout << 5 / x << '\n'; // divide by zero CRASH!!!!
+
+	cout << '\n';
 }
