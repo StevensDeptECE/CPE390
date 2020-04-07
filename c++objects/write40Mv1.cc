34a8c865c744b97dc954ef0081f953d36dee872f
Dov Kruger <Dov.Kruger@gmail.com>
2020-04-07 13:34:16 -0400

    session18 fast block I/O

--- c++objects/write40Mv1.cc
+++ c++objects/write40Mv1.cc
@@ -5,7 +5,6 @@ using namespace std;
 int main() {
 	const int n = 10000000;
 	ofstream f("test.dat");
-	Fraction a(1,2);
 	for (int i = 0; i < n; i++) {
 		f << '1' << '/' << '2'  << '\n';
 	}
--- c++objects/write40Mv3.cc
+++ c++objects/write40Mv3.cc
@@ -5,14 +5,15 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main(int argc, char* argv[]) {
 	const int n = 10000000;
 	int fh = open("test.dat", O_WRONLY);
 	if (fh < 0) {
 		cerr << "Could not open file";
 		return -1;
 	}
-	const int SIZE = 32768;
+	
+	int SIZE = atoi(argv[1]);
 	char buffer[SIZE];
 
 	int c = 0;
@@ -29,3 +30,12 @@ int main() {
 	write(fh, buffer, c); // write the last fractional part
 	close(fh);
 }
+
+/*
+ to go faster yet (hard)
+
+ 1. write to the buffer
+ 2. when it's full, write to disk IN ANOTHER THREAD.
+ 3. while that's going on, use another buffer and keep writing
+*/
+
