3ede0df6ed54bd3227d4f24455c45c39805a76b8
Dov Kruger <dov.kruger@stevens.edu>
2020-10-01 16:24:23 -0400

    session08 and 09

---
+++ 2020F/session08/compiling.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int f1(int a, int b) {
+	return a & b;
+}
+
+int f2(int a, int b) {
+	return a | b;
+}
+
+int f3(int a, int b) {
+	return a + b;
+}
+
+int f4(int a, int b) {
+	return a * b;
+}
+
+
+
+int main() {
+
+}
