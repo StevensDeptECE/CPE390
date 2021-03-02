69e94176cd003e7c1b010335b8b9db1a08c86c9d
Dov Kruger <dov.kruger@stevens.edu>
2021-03-02 14:00:15 -0500

    session09 and homework file

---
+++ 2021S/09loops/00loop.s
@@ -0,0 +1,12 @@
+	.global main
+main:
+	@ for (int i = 0 ; i < 100; i++) ;
+
+	mov	r0, #0
+loop:
+	add	r0, r0, #1
+	cmp	r0, #100   @ 	N=1  Z=0 V=1 C=0
+	bne	loop	   @ blt is the same for this loop	    bx	    lr
+	
+
+
---
+++ 2021S/09loops/makeexample/sum.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int sum(int v);
+
+int main() {
+	int y = sum(101);
+	cout << y << '\n';
+}
