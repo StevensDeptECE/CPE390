cf12344606171e7f3b16a14d2870ee6374ba2325
Dov Kruger <Dov.Kruger@stevens.edu>
2021-09-14 16:22:15 -0400

    session04

---
+++ 2021F/session04/00float.cc
@@ -0,0 +1,37 @@
+#include <cstdio>
+#include <iomanip>
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  uint64_t a = 1ULL + 1234567890123ULL;
+  float f = 1.2345678f;
+  double d = 1.23456789012345;
+  long double x = 1.2345678901234567890L;
+
+
+  d = 123.456789012345;
+  d =  6.022e+23; // 602200000000000000000000.
+  printf("%f %lf", f, d);
+  //  __float128
+
+  // seeeeeeeemmmmmmmmmmmmmmmmmmmmmmm
+  // seeeeeeeeeee
+
+  //  101.000000000
+  //  .101
+  //  1/2 + 1/8 = .625
+
+  // .2 2/10
+  // .1 1/10
+  // 1/3 = .33333333
+
+  cout << '\n';
+  cout << setprecision(8);
+  for (float x = 0; x <= 10; x += .1)
+    cout << x << ' ';
+  cout << '\n';
+
+  
+}
