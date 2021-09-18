833504fe11ca82374f3178fd61bc0d47aae12789
Dov Kruger <Dov.Kruger@stevens.edu>
2021-09-18 15:12:06 -0400

    fix my code2, revenge of the code

---
+++ hw/fixmycode02.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+float sumInverses(int n) {
+	float sum = 0;
+	for (int i = 0; i <= n; i++)
+		sum += 1/i;
+	return sum;
+}
+
+double mean(const double x[], int n) {
+	double sum = 0;
+	for (int i = 0; i <= n; i++)
+		sum += x[i];
+	return sum / n;
+}
+
+int main() {
+	cout << sumInverses(100) << '\n'; // should be 5.18...
+	double a[] = {6, 5, 4};
+	double b[] = {9, 1, 9, 1};
+	cout << mean(a, 3) << '\n';
+	cout << mean(b, 4) << '\n';
+}
