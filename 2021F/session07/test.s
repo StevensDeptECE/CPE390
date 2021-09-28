9d7c18416db377bf1d0656abd5aac11e9f27aa1f
Dov Kruger <Dov.Kruger@stevens.edu>
2021-09-28 18:28:41 -0400

    session 07 base conversion, getting ready for assembler programming

---
+++ 2021F/session07/test.s
@@ -0,0 +1,69 @@
+	.file	"test.cc"
+	.text
+	.section	.text.startup,"ax",@progbits
+	.p2align 4
+	.globl	main
+	.type	main, @function
+main:
+.LFB1810:
+	.cfi_startproc
+	endbr64
+	subq	$8, %rsp
+	.cfi_def_cfa_offset 16
+	movl	$3, %edi
+	call	_Z1fi@PLT
+	leaq	_ZSt4cout(%rip), %rdi
+	movl	%eax, %esi
+	call	_ZNSolsEi@PLT
+	xorl	%eax, %eax
+	addq	$8, %rsp
+	.cfi_def_cfa_offset 8
+	ret
+	.cfi_endproc
+.LFE1810:
+	.size	main, .-main
+	.p2align 4
+	.type	_GLOBAL__sub_I_main, @function
+_GLOBAL__sub_I_main:
+.LFB2292:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	leaq	_ZStL8__ioinit(%rip), %rbp
+	movq	%rbp, %rdi
+	call	_ZNSt8ios_base4InitC1Ev@PLT
+	movq	_ZNSt8ios_base4InitD1Ev@GOTPCREL(%rip), %rdi
+	movq	%rbp, %rsi
+	popq	%rbp
+	.cfi_def_cfa_offset 8
+	leaq	__dso_handle(%rip), %rdx
+	jmp	__cxa_atexit@PLT
+	.cfi_endproc
+.LFE2292:
+	.size	_GLOBAL__sub_I_main, .-_GLOBAL__sub_I_main
+	.section	.init_array,"aw"
+	.align 8
+	.quad	_GLOBAL__sub_I_main
+	.local	_ZStL8__ioinit
+	.comm	_ZStL8__ioinit,1,1
+	.hidden	__dso_handle
+	.ident	"GCC: (Ubuntu 11.1.0-1ubuntu1~20.04) 11.1.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	1f - 0f
+	.long	4f - 1f
+	.long	5
+0:
+	.string	"GNU"
+1:
+	.align 8
+	.long	0xc0000002
+	.long	3f - 2f
+2:
+	.long	0x3
+3:
+	.align 8
+4:
