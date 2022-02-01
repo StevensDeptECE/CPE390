73eb3030aa1486950be84e377dbf465f0015e60f
Dov Kruger <Dov.Kruger@stevens.edu>
2022-02-01 13:44:50 -0500

    session4 floating point

---
+++ 2022S/04_float/02bugs.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+	/*
+		101010101.101010101011110111
+             .000000010111010101011010101
+================================
+
+
+	 */
+
+
+	
+	cout << setprecision(8);
+	for (float f = 100000100.0 ; f <= 100000100.0; f += 1)
+		cout << f << ' ';
+	cout << '\n';
+
+	return 0;
+}
---
+++ 2022S/04_float/04summation.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	float sum = 0;
+	for (float i = 1; i < 10; i++)
+		sum += 1.0/i;
+	cout << sum << '\n';
+}
