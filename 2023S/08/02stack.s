604e9b1b6c0f5082a83c84103019232826b7422e
Dov Kruger <Dov.Kruger@stevens.edu>
2023-03-02 14:07:47 -0500

    added overview for test1

--- 2023S/08/02stack.s
+++ 2023S/08/02stack.s
@@ -17,10 +17,23 @@ f:
   mov  %rsp, %rdx  # rdx is pointing just beyond the end of the array of 32 bytes
   sub  $32, %rsp
   mov  %rsp, %rcx  # rcx is pointing to our "array" of 32 bytes
+
+  # let's first write 0 to each byte in the 32-bytes allocated
 loop:
   movb $0, (%rcx)  # equivalent to x[i] = 0 if x is an array of char...
   add  $1, %rcx
   cmp  %rdx, %rcx
   jl   loop
+
+  # this is faster, in only 4 times we can write all 32 bytes
+  mov  %rsp, %rcx   # start back at the beginning again!
+loop2:
+  movq $5, (%rcx)   # 0x0000000000000005
+  add  $8, %rcx  
+  cmp  %rdx, %rcx
+  jl   loop2
+
+
+
   add  $32, %rsp
   ret
